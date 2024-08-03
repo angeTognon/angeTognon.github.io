28d06759ee0ec1d67f652524e8f4ea8bd39b3687
Ange tognon <101118344+angeTognon@users.noreply.github.com>
2024-08-03 14:30:22 +0100

    hj5878

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"235db911ba279722f5e685f38b0ed30fa7e857
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "373182250"
+    serviceWorkerVersion: "2276726709"
   }
 });
